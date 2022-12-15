ad79d974c60edf2e33bb7b8bd99bab4723bddd14
Андрей <andrej@MacBook-Air.local>
2022-12-15 19:14:22 +0300

    fix:fixed command rn

--- fs/renameFile.js
+++ fs/renameFile.js
@@ -1,11 +1,12 @@
 import path from "path";
 import * as fsPromise from "fs/promises";
 
-export const renameFile = async (oldNameFile, newFileName) => {
+export const renameFile = async (pathOldNameFile, newFileName) => {
+  const pathToFile = path.isAbsolute(pathOldNameFile) ? pathOldNameFile : path.join(process.cwd(), pathOldNameFile);
   try {
-    await fsPromise.rename(path.join(process.cwd(), oldNameFile), path.join(process.cwd(), newFileName));
+    await fsPromise.rename(pathToFile, newFileName);
     console.log("File renamed successfully!");
   } catch (error) {
-    console.log("Error renaming file");
+    console.log(` ${pathOldNameFile} no such file exists`);
   }
 };
