72db97a874c0a0da5e9245185e5f7af4dd7adad5
Андрей <andrej@MacBook-Air.local>
2022-12-15 19:21:36 +0300

    fix:fixed command cp

--- fs/copyFile.js
+++ fs/copyFile.js
@@ -2,10 +2,22 @@ import * as fsPromise from "fs/promises";
 import fs from "fs";
 import { pipeline } from "stream/promises";
 import path from "path";
+import { getDataPath } from "../helper";
 
 export const copyFile = async (pathFile, pathDirCopyFile) => {
   try {
-    await fsPromise.stat(path.join(pathFile));
+    const { isDirectory, error } = await getDataPath(pathFile);
+
+    if (isDirectory) {
+      console.log(`${pathFile} is not a file`);
+      return;
+    }
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+
     const nameFile = path.basename(pathFile);
     const readStream = fs.createReadStream(path.join(pathFile), { flags: "r" });
     const writeStream = fs.createWriteStream(path.join(pathDirCopyFile, nameFile), { flags: "wx" });
