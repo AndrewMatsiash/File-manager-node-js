abc18e6d828a3cad3b885937e97e0c3c5e220907
Андрей <andrej@MacBook-Air.local>
2022-12-18 15:07:25 +0300

    fix:fixed command decompress

--- zlib/decompress.js
+++ zlib/decompress.js
@@ -1,15 +1,41 @@
 import zlib from "zlib";
-import path from "zlib";
-import * as fsPromise from "fs/promises";
+import path from "path";
+import fs from "fs";
+import { getDataPath } from "../helper.js";
+import { pipeline } from "stream/promises";
 
-export const decompressFile = async (pathDecompressFile, pathToDir) => {
+export const decompressFile = async (pathDecompressFile, pathDecompressToDir) => {
   try {
-    await fsPromise.stat(path.join(pathDecompressFile));
-    const nameFile = path.basename(pathDecompressFile).replace(".br", "");
-    const readStream = fs.createReadStream(path.join(pathDecompressFile), { flags: "r" });
+    const { isFile, error: pathFileError } = await getDataPath(pathDecompressFile);
+    const { isDirectory, error: movePathDirError } = await getDataPath(pathDecompressToDir);
+
+    if (!isFile) {
+      console.log(`${pathDecompressToDir} is not a file`);
+      return;
+    }
+
+    if (!isDirectory) {
+      console.log(`${pathDecompressToDir} is not a directory`);
+      return;
+    }
+
+    if (movePathDirError || pathFileError) {
+      console.log("wrong path");
+      return;
+    }
+
+    const pathToFile = path.isAbsolute(pathDecompressFile)
+      ? pathDecompressFile
+      : path.join(process.cwd(), pathDecompressFile);
+    const pathToDir = path.isAbsolute(pathDecompressToDir)
+      ? pathDecompressToDir
+      : path.join(process.cwd(), pathDecompressToDir);
+
+    const nameFile = path.basename(pathToFile).replace(".br", "");
+    const readStream = fs.createReadStream(pathToFile, { flags: "r" });
     const writeStream = fs.createWriteStream(path.join(pathToDir, nameFile), { flags: "w" });
-    const brotliCompress = zlib.brotliDecompress();
-    await pipeline(readStream, brotliCompress, writeStream);
+    const brotliDecompress = zlib.createBrotliDecompress();
+    await pipeline(readStream, brotliDecompress, writeStream);
   } catch {
     console.log("wong path");
   }
